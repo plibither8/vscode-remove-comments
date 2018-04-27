a6922148f6a8311556b42af7f97a45c8dde99200
Mihir Chaturvedi <plibither8@gmail.com>
2018-04-27 22:42:40 +0530

    Single line comment deletion almost working

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,40 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Parser } from './parser';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "remove-comments" is now active!');
+    let activeEditor: vscode.TextEditor;
+    let parser: Parser = new Parser();
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    let removeComments = function () {
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        if (!activeEditor) {
+            return;
+        }
+        if (!parser.supportedLanguage) {
+            return;
+        }
+
+        parser.FindSingleLineComments(activeEditor);
+
+    }
+
+
+    let removeCommentsCommand = vscode.commands.registerCommand('extension.removeComments', () => {
+
+        if (vscode.window.activeTextEditor) {
+
+            activeEditor = vscode.window.activeTextEditor;
+            parser.SetRegex(activeEditor.document.languageId);
+            removeComments();
+
+        }
+
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(removeCommentsCommand);
+
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
-}
+
+export function deactivate() { }
--- src/parser.ts
+++ src/parser.ts
@@ -1,123 +1,44 @@
 import * as vscode from 'vscode';
 
-interface CommentTag {
-    tag: string;
-    escapedTag: string;
-    ranges: Array<vscode.DecorationOptions>;
-}
 
 export class Parser {
-    private tags: CommentTag[] = [];
     private expression: string = "";
     private delimiter: string = "";
 
-    // * this is used to trigger the events when a supported language code is found
     public supportedLanguage = true;
 
-	/**
-	 * Sets the regex to be used by the matcher based on the config specified in the package.json
-	 * @param languageCode The short code of the current language
-	 * https://code.visualstudio.com/docs/languages/identifiers
-	 */
     public SetRegex(languageCode: string) {
         this.setDelimiter(languageCode);
 
-        let characters: Array<string> = [];
-        for (let commentTag of this.tags) {
-            characters.push(commentTag.escapedTag);
-        }
-
-        // start by finding the delimiter (//, --, #, ') with optional spaces or tabs
         this.expression = "(" + this.delimiter.replace(/\//ig, "\\/") + ")+( |\t)*";
 
-        // Apply all configurable comment start tags
         this.expression += "(";
-        this.expression += characters.join("|");
         this.expression += ")+(.*)";
     }
 
-	/**
-	 * Finds all single line comments delimted by a given delimter and matching tags specified in package.json
-	 * @param activeEditor  The active text editor containing the code document
-	 */
-    public FindSingleLineComments(activeEditor: vscode.TextEditor): void {
+    public FindSingleLineComments(activeEditor: vscode.TextEditor): any {
+
+        // console.log("yay");
         let text = activeEditor.document.getText();
         let regEx = new RegExp(this.expression, "ig");
 
         let match: any;
         while (match = regEx.exec(text)) {
+
+            let edit = new vscode.WorkspaceEdit();
+        
+            // console.log(match);
             let startPos = activeEditor.document.positionAt(match.index);
             let endPos = activeEditor.document.positionAt(match.index + match[0].length);
-            let range = { range: new vscode.Range(startPos, endPos) };
+            let range = new vscode.Range(startPos, endPos);
+            console.log(range);
+            edit.delete(activeEditor.document.uri, range);
+            // console.log(range);
+            vscode.workspace.applyEdit(edit);
 
-            // Find which custom delimiter was used in order to add it to the collection
-            let matchTag = this.tags.find(item => item.tag.toLowerCase() === match[3].toLowerCase());
-
-            if (matchTag) {
-                matchTag.ranges.push(range);
-            }
         }
     }
 
-	/**
-	 * Finds all multiline comments starting with /*
-	 * @param activeEditor The active text editor containing the code document
-	 */
-    public FindMultilineComments(activeEditor: vscode.TextEditor, findJSDoc: boolean = false): void {
-
-        let text = activeEditor.document.getText();
-
-        // Build up regex matcher for custom delimter tags
-        let characters: Array<string> = [];
-        for (let commentTag of this.tags) {
-            characters.push(commentTag.escapedTag);
-        }
-
-        // Combine custom delimiters and the rest of the comment block matcher
-        let commentMatchString: string = "";
-        let regEx: RegExp;
-
-        if (findJSDoc) {
-            commentMatchString = "(^)+([ \\t]*\\*[ \\t]*)("; // Highlight after leading *
-            regEx = /(^|[ \t])(\/\*\*)+([\s\S]*?)(\*\/)/gm; // Find rows of comments matching pattern /** */		
-        } else {
-            commentMatchString = "(^)+([ \\t]*[ \\t]*)("; // Don't expect the leading *
-            regEx = /(^|[ \t])(\/\*[^*])+([\s\S]*?)(\*\/)/gm; // Find rows of comments matching pattern /* */
-        }
-
-        commentMatchString += characters.join("|");
-        commentMatchString += ")([ ]*|[:])+([^*/][^\\r\\n]*)";
-
-        let commentRegEx = new RegExp(commentMatchString, "igm");
-
-        // Find the multiline comment block
-        let match: any;
-        while (match = regEx.exec(text)) {
-            let commentBlock = match[0];
-
-            // Find the line
-            let line;
-            while (line = commentRegEx.exec(commentBlock)) {
-                let startPos = activeEditor.document.positionAt(match.index + line.index + line[2].length);
-                let endPos = activeEditor.document.positionAt(match.index + line.index + line[0].length);
-                let range: vscode.DecorationOptions = { range: new vscode.Range(startPos, endPos) };
-
-                // Find which custom delimiter was used in order to add it to the collection
-                let matchString = line[3] as string;
-                let matchTag = this.tags.find(item => item.tag.toLowerCase() === matchString.toLowerCase());
-
-                if (matchTag) {
-                    matchTag.ranges.push(range);
-                }
-            }
-        }
-    }
-
-	/**
-	 * Sets the comment delimiter [//, #, --, '] of a given language
-	 * @param languageCode The short code of the current language
-	 * https://code.visualstudio.com/docs/languages/identifiers
-	 */
     private setDelimiter(languageCode: string): void {
         this.supportedLanguage = true;
 
